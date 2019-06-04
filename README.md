5d2e39df3bca5ca16679628ec43e3a15e68a6200
vlabo <vladdocan@gmail.com>
2019-06-04 12:27:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Pandoc C++ lua filter
+
+A simple pandoc filter that compiles and runs c++ code from markdown code block and add the result to the output document.
+
+### Example use:  
+`pandoc --lua-filter cpp.lua test.md -o test.html`
+
+### Requirements:
+* Pandoc 2
+* POSIX OS (Linux, Mac, ...)
+* GCC
+
+### Usege:
+* hide: remove the code block from the output
+* ignore: those not execute the code
+* global: those not execute the code. Adds it to a global file that is include to each subsequent code block.
+
+
+```markdown
+This code will NOT be executed but added to the next block
+\`\`\`{.cpp global="ture"}
+
+#include <iostream>
+using namespace std;
+
+\`\`\`
+
+This Code will be executed
+\`\`\`cpp
+int main()
+{
+	cout << "Hello World" << endl;
+}
+\`\`\`
+**Result**: Hello World
+
+This code will be ignored and hiden from the output
+\`\`\`{.cpp hide="true" ignore="true"}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Ignored result" << endl;
+}
+\`\`\`
+```
---
+++ cpp.lua
@@ -0,0 +1,104 @@
+--- MIT License (MIT)
+--- Copyright (c) 2019. Vladimir Stoilov
+--- Permission is hereby granted, free of charge, to any person obtaining a copy
+--- of this software and associated documentation files (the "Software"), to deal
+--- in the Software without restriction, including without limitation the rights
+--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--- copies of the Software, and to permit persons to whom the Software is
+--- furnished to do so, subject to the following conditions:
+--- The above copyright notice and this permission notice shall be included in
+--- all copies or substantial portions of the Software.
+--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+--- THE SOFTWARE.
+
+--- File        : cpp.lua
+--- Author      : Vladimir Stoilov
+--- Description : Compiles and runs and prints the result of c++ code inside markdown document.
+
+cppError = ""
+globalCode = ""
+
+local function has_value (tab, val)
+   for index, value in ipairs(tab) do
+      if value == val then
+	 return true
+      end
+   end
+
+   return false
+end
+
+wordcount = {	
+   CodeBlock = function(el)
+      cppError = ""
+      local out = ""
+
+      local isGlobal = false
+      local hide = false
+      local ignore = false
+      
+      if not has_value( el.classes, "cpp") then
+	 return el
+      end
+            
+      if el.attributes["ignore"] == "true" then
+	 ignore = true
+      end
+
+      if el.attributes["global"] == "true" then
+	 isGlobal = true
+      end
+
+      if el.attributes["hide"] == "true" then
+	 hide = true
+      end
+      
+      if not ignore and not isGlobal then
+	 local cppFileName = '/tmp/pandoc_tmp.cpp'  
+	 local f = io.open( cppFileName, 'w')
+	 f:write(globalCode)
+	 f:write("\n")
+	 f:write(el.text)
+	 f:close()
+
+	 local exeName = '/tmp/pandoc_cpp_tmp'
+	 cppError = pandoc.pipe('g++', {cppFileName, '-o', exeName}, '')
+	 if cppError == "" then
+	    out = pandoc.pipe(exeName, {}, '')
+	 else
+	    print(cppError)
+	 end
+
+	 pandoc.pipe('rm', {cppFileName, exeName}, '')
+      end
+
+      if isGlobal and not ignore then
+	 globalCode = globalCode .. "\n" .. el.text
+      end
+      
+      local result = {}
+
+      if not hide then
+	 table.insert(result, el)
+      end
+
+      if out ~= "" then
+	 table.insert(result, pandoc.Para({pandoc.Strong("Result: "), pandoc.Str(out)})) 
+      end
+      
+      return result
+   end,
+}
+
+function Pandoc(el)
+   local div = pandoc.Div(el.blocks)
+   local pan = pandoc.walk_block(div, wordcount).content
+   return pandoc.Pandoc(pan, el.meta)
+end
+	   
+
